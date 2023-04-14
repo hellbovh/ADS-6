515786cf9ff0957d399497f6b51ae63a30737bab
hellbovh <69521565+hellbovh@users.noreply.github.com>
2023-04-15 00:05:54 +0300

    Update tpqueue.h

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -6,63 +6,63 @@
 template<typename T, int size>
 class TPQueue {
  private:
-    T* arr;
-    int first, last, count;
+  T* arr;
+  int first, last, count;
  public:
-    TPQueue() : first(0), last(0), count(0) {
-        arr = new T[size];
-    }
-    bool isEmpty() const;
-    bool isFull() const;
-    void push(const T& value);
-    const T& pop();
+  TPQueue() : first(0), last(0), count(0) {
+    arr = new T[size];
+  }
+  bool isEmpty() const;
+  bool isFull() const;
+  void push(const T& value);
+  const T& pop();
 };
 
 template <typename T, int size>
 bool TPQueue<T, size>::isEmpty() const {
-    return count == 0;
+  return count == 0;
 }
 
 template <typename T, int size>
 bool TPQueue<T, size>::isFull() const {
-    return count == size;
+  return count == size;
 }
 
 template <typename T, int size>
 void TPQueue<T, size>::push(const T& value) {
-    if (isFull()) {
-        throw std::string("Queue is full!");
-    } else {
-        count++;
-        int t_l = last;
-        arr[last++ % size] = value;
-        while (t_l > first && arr[t_l % size].prior > arr[(t_l - 1) % size].prior) {
-            T temp = arr[t_last % size];
-            arr[t_last % size] = arr[(t_last - 1) % size];
-            arr[(t_last - 1) % size] = temp;
-            t_last--;
-        }
+  if (isFull()) {
+    throw std::string("Queue is full!");
+  } else {
+    count++;
+    int t_l = last;
+    arr[last++ % size] = value;
+    while (t_l > first && arr[t_l % size].prior > arr[(t_l - 1) % size].prior) {
+      T temp = arr[t_l % size];
+      arr[t_l % size] = arr[(t_l - 1) % size];
+      arr[(t_l - 1) % size] = temp;
+      t_l--;
     }
+  }
 }
 
 template <typename T, int size>
 const T& TPQueue<T, size>::pop() {
-    if (isEmpty()) {
-        throw std::string("Queue is empty!");
-    } else {
-        T& data = arr[first];
-        first++;
-        count--;
-        if (first > size) {
-            first -= size;
-        }
-        return data;
+  if (isEmpty()) {
+    throw std::string("Queue is empty!");
+  } else {
+    T& data = arr[first];
+    first++;
+    count--;
+    if (first > size) {
+      first -= size;
     }
+    return data;
+  }
 }
 
 struct SYM {
-    char ch;
-    int prior;
+  char ch;
+  int prior;
 };
 
 #endif  // INCLUDE_TPQUEUE_H_
